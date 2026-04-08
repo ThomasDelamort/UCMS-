473d42e9a637a3edc05cff23b2db1c1f064619b7
Christian Neal Paredes <nealparedes776@gmail.com>
2026-04-08 09:14:42 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -155,3 +155,154 @@ public class Main {
                 }
                 default -> System.out.println("Invalid Choice!");
             }
+        }
+    }
+
+    static void assignModuleToCourse() {
+        if (courses.isEmpty()) {
+            System.out.println("No Courses\n");
+        } else {
+            modulesListDisplay();
+            System.out.println("Enter Module Index: ");
+            int mIndex = sc.nextInt(); sc.nextLine();
+            mIndex -= 1;
+
+            courseListDisplay();
+            System.out.println("Enter Course Index: ");
+            int clIndex = sc.nextInt(); sc.nextLine();
+            clIndex -= 1;
+
+            if (clIndex >= 0 && clIndex < courses.size()
+                    && mIndex >= 0 && mIndex < modules.size()) {
+                courses.get(clIndex).addModule(modules.get(mIndex));
+                System.out.println("Module assigned!\n");
+            } else {
+                System.out.println("Invalid index\n");
+            }
+        }
+    }
+
+    static void enrollStudentToCourse() {
+        if (registeredStudents.isEmpty()) {
+            System.out.println("No students\n");
+        } else if (courses.isEmpty()) {
+            System.out.println("No Courses\n");
+        } else {
+            displayStudents();
+            System.out.print("Select Student: ");
+            int sIndex = sc.nextInt(); sc.nextLine();
+            sIndex -= 1;
+
+            if (sIndex < 0 || sIndex >= registeredStudents.size()) {
+                System.out.println("Invalid student\n");
+                return;
+            }
+
+            courseListDisplay();
+            System.out.print("Select Course: ");
+            int cIndex = sc.nextInt(); sc.nextLine();
+            cIndex -= 1;
+
+            if (cIndex < 0 || cIndex >= courses.size()) {
+                System.out.println("Invalid course\n");
+            } else {
+                courses.get(cIndex).enrollStudent(registeredStudents.get(sIndex));
+                System.out.println("Student enrolled!\n");
+            }
+        }
+    }
+
+    static void displayAll() {
+        if (departments.isEmpty()) {
+            System.out.println("No data\n");
+            return;
+        }
+        for (Department d : departments) {
+            System.out.println("\nDepartment: " + d.getDepartmentName());
+
+            if (d.getCourses().isEmpty()) {
+                System.out.println(" No courses");
+            } else {
+                for (Course c : d.getCourses()) {
+                    c.displayCourse();
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
+    static void displayStudents() {
+        System.out.println("\n===== Students =====");
+        if (registeredStudents.isEmpty()) {
+            System.out.println("No students\n");
+            return;
+        }
+        for (int i = 0; i < registeredStudents.size(); i++) {
+            System.out.println(i+1 + ". " + registeredStudents.get(i).getStudentName());
+        }
+        System.out.println();
+    }
+
+    static void displayDepartments() {
+        System.out.println("\n===== Departments =====");
+        for (int i = 0; i < departments.size(); i++) {
+            System.out.println(i+1 + ". " + departments.get(i).getDepartmentName());
+        }
+        System.out.println();
+    }
+
+    static void displayCourses(Department dept) {
+        System.out.println("\n===== Courses =====");
+        for (int i = 0; i < dept.getCourses().size(); i++) {
+            System.out.println(i+1 + ". " + dept.getCourses().get(i).getCourseName());
+        }
+        System.out.println();
+    }
+
+    static void courseListDisplay() {
+        System.out.println("\n===== Courses =====");
+        for (int i = 0; i < courses.size(); i++) {
+            System.out.println(i+1 + ". " + courses.get(i).getCourseName());
+        }
+    }
+
+    static void modulesListDisplay() {
+        System.out.println("\n===== Modules =====");
+        for (int i = 0; i < modules.size(); i++) {
+            System.out.println(i+1 + ". " + modules.get(i).getModuleName());
+        }
+    }
+
+    static void menu() {
+        System.out.println("""
+                    ==== University Management System ====
+                    1. Register Student
+                    2. Add Department
+                    3. Add Course
+                    4. Assign Course to Department
+                    5. Add Module
+                    6. Assign Module to Course
+                    7. Enroll Student to Course
+                    8. Display All Information
+                    9. Display Students
+                    10. Exit
+                    """);
+    }
+
+    static void menuAdd() {
+        System.out.println("""
+                    ==== University Management System ====
+                    1. Register Student
+                    2. Add Department
+                    3. Add Course
+                    4. Assign Course to Department
+                    5. Add Module (Unavailable)
+                    6. Assign Module to Course (Unavailable)
+                    7. Enroll Student to Course
+                    8. Display All Information
+                    9. Display Students
+                    10. Exit
+                    """);
+    }
+}
